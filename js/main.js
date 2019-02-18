c18632cc3608d9f7606fdf8c204946908b27faba
devinberry2015 <47096212+devinberry2015@users.noreply.github.com>
2019-02-17 22:47:55 -0600

    site update

--- js/main.js
+++ js/main.js
@@ -1,405 +1,119 @@
-$(document).ready(function () {
+$(function() {
   "use strict";
 
-  var window_width = $(window).width(),
-    window_height = window.innerHeight,
-    header_height = $(".default-header").height(),
-    header_height_static = $(".site-header.static").outerHeight(),
-    fitscreen = window_height - header_height;
-
-
-  // $(".fullscreen").css("height", window_height)
-  // $(".fitscreen").css("height", fitscreen);
-
-  //-------- Active Sticky Js ----------//
-  $(".default-header").sticky({
-    topSpacing: 0
+  //------- Parallax -------//
+  skrollr.init({
+    forceHeight: false
   });
 
+  //------- Active Nice Select --------//
+  $('select').niceSelect();
 
-  if (document.getElementById("default-select")) {
-    $('select').niceSelect();
-  };
-
-  /*----------------------------------------------------*/
-  /*  Magnific Pop up js (Home Video)
-  /*----------------------------------------------------*/
-  $('#play-home-video').magnificPopup({
-    type: 'iframe',
-    mainClass: 'mfp-fade',
-    removalDelay: 160,
-    preloader: false,
-    fixedContentPos: false
-  });
-
-  $('.img-pop-up').magnificPopup({
-    type: 'image',
-    gallery: {
-      enabled: true
-    }
-  });
-
-
-
-  // $('.navbar-nav>li>a').on('click', function(){
-  //     $('.navbar-collapse').collapse('hide');
-  // });
-  $('.active-testimonial').owlCarousel({
-    items: 3,
-    margin: 30,
-    // autoplay: true,
-    loop: true,
-    dots: true,
-    responsive: {
-      0: {
-        items: 1
+  //------- hero carousel -------//
+  $(".hero-carousel").owlCarousel({
+    items:3,
+    margin: 10,
+    autoplay:false,
+    autoplayTimeout: 5000,
+    loop:true,
+    nav:false,
+    dots:false,
+    responsive:{
+      0:{
+        items:1
       },
-      480: {
-        items: 1,
+      600:{
+        items: 2
       },
-      991: {
-        items: 2,
-      },
-      1200: {
-        items: 3,
+      810:{
+        items:3
       }
-
     }
   });
 
-  $('.active-brand-carusel').owlCarousel({
-    items: 5,
-    loop: true,
-    margin: 30,
-    autoplayHoverPause: true,
-    smartSpeed:650,         
-    autoplay:true, 
-    responsive: {
-        0: {
-            items: 1
+  //------- Best Seller Carousel -------//
+  if($('.owl-carousel').length > 0){
+    $('#bestSellerCarousel').owlCarousel({
+      loop:true,
+      margin:30,
+      nav:true,
+      navText: ["<i class='ti-arrow-left'></i>","<i class='ti-arrow-right'></i>"],
+      dots: false,
+      responsive:{
+        0:{
+          items:1
         },
-        480: {
-            items: 2,
+        600:{
+          items: 2
         },
-        768: {
-            items: 4,
+        900:{
+          items:3
         },
-        768: {
-            items: 5,
-        }
-    }
-});
-
-
-
-  //------- Filter  js --------//
-  $(window).on("load", function () {
-    $(".filters ul li").on("click", function () {
-      $(".filters ul li").removeClass("active");
-      $(this).addClass("active");
-
-      var data = $(this).attr("data-filter");
-      $grid.isotope({
-        filter: data
-      });
-    });
-
-    if (document.getElementById("project")) {
-      var $grid = $(".grid").isotope({
-        itemSelector: ".grid-item",
-        percentPosition: true,
-        masonry: {
-          columnWidth: ".grid-sizer"
-        }
-      });
-    }
-  });
-
-
-
-
-  // Select all links with hashes
-  $('.navbar-nav a[href*="#"]')
-    // Remove links that don't actually link to anything
-    .not('[href="#"]')
-    .not('[href="#0"]')
-    .on('click', function (event) {
-      // On-page links
-      if (
-        location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
-        location.hostname == this.hostname
-      ) {
-        // Figure out element to scroll to
-        var target = $(this.hash);
-        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-        // Does a scroll target exist?
-        if (target.length) {
-          // Only prevent default if animation is actually gonna happen
-          event.preventDefault();
-          $('html, body').animate({
-            scrollTop: target.offset().top - 50
-          }, 1000, function () {
-            // Callback after animation
-            // Must change focus!
-            var $target = $(target);
-            $target.focus();
-            if ($target.is(":focus")) { // Checking if the target was focused
-              return false;
-            } else {
-              $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
-              $target.focus(); // Set focus again
-            };
-          });
+        1130:{
+          items:4
         }
       }
-    });
-
-
-
-  // -------   Mail Send ajax
-
-  $(document).ready(function () {
-    var form = $('#booking'); // contact form
-    var submit = $('.submit-btn'); // submit button
-    var alert = $('.alert-msg'); // alert div for show alert message
-
-    // form submit event
-    form.on('submit', function (e) {
-      e.preventDefault(); // prevent default form submit
-
-      $.ajax({
-        url: 'booking.php', // form action url
-        type: 'POST', // form submit method get/post
-        dataType: 'html', // request type html/json/xml
-        data: form.serialize(), // serialize form data
-        beforeSend: function () {
-          alert.fadeOut();
-          submit.html('Sending....'); // change submit button text
-        },
-        success: function (data) {
-          alert.html(data).fadeIn(); // fade in response data
-          form.trigger('reset'); // reset form
-          submit.attr("style", "display: none !important");; // reset submit button text
-        },
-        error: function (e) {
-          console.log(e)
-        }
-      });
-    });
-  });
-
-  if ($("#mapBox").length) {
-    var $lat = $("#mapBox").data("lat");
-    var $lon = $("#mapBox").data("lon");
-    var $zoom = $("#mapBox").data("zoom");
-    var $marker = $("#mapBox").data("marker");
-    var $info = $("#mapBox").data("info");
-    var $markerLat = $("#mapBox").data("mlat");
-    var $markerLon = $("#mapBox").data("mlon");
-    var map = new GMaps({
-      el: "#mapBox",
-      lat: $lat,
-      lng: $lon,
-      scrollwheel: false,
-      scaleControl: true,
-      streetViewControl: false,
-      panControl: true,
-      disableDoubleClickZoom: true,
-      mapTypeControl: false,
-      zoom: $zoom,
-      styles: [
-        {
-          featureType: "water",
-          elementType: "geometry.fill",
-          stylers: [
-            {
-              color: "#dcdfe6"
-            }
-          ]
-        },
-        {
-          featureType: "transit",
-          stylers: [
-            {
-              color: "#808080"
-            },
-            {
-              visibility: "off"
-            }
-          ]
-        },
-        {
-          featureType: "road.highway",
-          elementType: "geometry.stroke",
-          stylers: [
-            {
-              visibility: "on"
-            },
-            {
-              color: "#dcdfe6"
-            }
-          ]
-        },
-        {
-          featureType: "road.highway",
-          elementType: "geometry.fill",
-          stylers: [
-            {
-              color: "#ffffff"
-            }
-          ]
-        },
-        {
-          featureType: "road.local",
-          elementType: "geometry.fill",
-          stylers: [
-            {
-              visibility: "on"
-            },
-            {
-              color: "#ffffff"
-            },
-            {
-              weight: 1.8
-            }
-          ]
-        },
-        {
-          featureType: "road.local",
-          elementType: "geometry.stroke",
-          stylers: [
-            {
-              color: "#d7d7d7"
-            }
-          ]
-        },
-        {
-          featureType: "poi",
-          elementType: "geometry.fill",
-          stylers: [
-            {
-              visibility: "on"
-            },
-            {
-              color: "#ebebeb"
-            }
-          ]
-        },
-        {
-          featureType: "administrative",
-          elementType: "geometry",
-          stylers: [
-            {
-              color: "#a7a7a7"
-            }
-          ]
-        },
-        {
-          featureType: "road.arterial",
-          elementType: "geometry.fill",
-          stylers: [
-            {
-              color: "#ffffff"
-            }
-          ]
-        },
-        {
-          featureType: "road.arterial",
-          elementType: "geometry.fill",
-          stylers: [
-            {
-              color: "#ffffff"
-            }
-          ]
-        },
-        {
-          featureType: "landscape",
-          elementType: "geometry.fill",
-          stylers: [
-            {
-              visibility: "on"
-            },
-            {
-              color: "#efefef"
-            }
-          ]
-        },
-        {
-          featureType: "road",
-          elementType: "labels.text.fill",
-          stylers: [
-            {
-              color: "#696969"
-            }
-          ]
-        },
-        {
-          featureType: "administrative",
-          elementType: "labels.text.fill",
-          stylers: [
-            {
-              visibility: "on"
-            },
-            {
-              color: "#737373"
-            }
-          ]
-        },
-        {
-          featureType: "poi",
-          elementType: "labels.icon",
-          stylers: [
-            {
-              visibility: "off"
-            }
-          ]
-        },
-        {
-          featureType: "poi",
-          elementType: "labels",
-          stylers: [
-            {
-              visibility: "off"
-            }
-          ]
-        },
-        {
-          featureType: "road.arterial",
-          elementType: "geometry.stroke",
-          stylers: [
-            {
-              color: "#d6d6d6"
-            }
-          ]
-        },
-        {
-          featureType: "road",
-          elementType: "labels.icon",
-          stylers: [
-            {
-              visibility: "off"
-            }
-          ]
-        },
-        {},
-        {
-          featureType: "poi",
-          elementType: "geometry.fill",
-          stylers: [
-            {
-              color: "#dadada"
-            }
-          ]
-        }
-      ]
-    });
+    })
   }
 
-  $("select").niceSelect();
-
-
-  $(document).ready(function () {
-    $('#mc_embed_signup').find('form').ajaxChimp();
+  //------- single product area carousel -------//
+  $(".s_Product_carousel").owlCarousel({
+    items:1,
+    autoplay:false,
+    autoplayTimeout: 5000,
+    loop:true,
+    nav:false,
+    dots:false
   });
 
+  //------- mailchimp --------//  
+	function mailChimp() {
+		$('#mc_embed_signup').find('form').ajaxChimp();
+	}
+  mailChimp();
+  
+  //------- fixed navbar --------//  
+  $(window).scroll(function(){
+    var sticky = $('.header_area'),
+    scroll = $(window).scrollTop();
+
+    if (scroll >= 100) sticky.addClass('fixed');
+    else sticky.removeClass('fixed');
+  });
+
+  //------- Price Range slider -------//
+  if(document.getElementById("price-range")){
+  
+    var nonLinearSlider = document.getElementById('price-range');
+    
+    noUiSlider.create(nonLinearSlider, {
+        connect: true,
+        behaviour: 'tap',
+        start: [ 500, 4000 ],
+        range: {
+            // Starting at 500, step the value by 500,
+            // until 4000 is reached. From there, step by 1000.
+            'min': [ 0 ],
+            '10%': [ 500, 500 ],
+            '50%': [ 4000, 1000 ],
+            'max': [ 10000 ]
+        }
+    });
+  
+  
+    var nodes = [
+        document.getElementById('lower-value'), // 0
+        document.getElementById('upper-value')  // 1
+    ];
+  
+    // Display the slider value and how far the handle moved
+    // from the left edge of the slider.
+    nonLinearSlider.noUiSlider.on('update', function ( values, handle, unencoded, isTap, positions ) {
+        nodes[handle].innerHTML = values[handle];
+    });
+  
+  }
+  
 });
+
+
